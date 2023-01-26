7d301391f1df32766db13ffada4f12cca6da12c7
CrazyPassion218 <classicmusic218@gmail.com>
2023-01-26 21:52:03 +0900

    -public directory cleaned. and control the orbit radius when user click annotation description bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Children } from "react";
 import './App.css';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useGLTF } from "@react-three/drei";
 import { useLoader } from '@react-three/fiber'
 import {AppLayout} from "./AppLayout";
 
 function App() {
     const obj = useLoader(OBJLoader, 'human_model.obj');
-
+    
     return (
         <AppLayout
             model={obj}
