faad1876ec5403cf6a522cbef1a21ed10701f1af
Henry David Thoruea <classicmusic218@gmail.com>
2023-02-01 23:38:35 +0900

    -define constant related to heatmap and area radius and use it dynamically. -second camera system updated finally.

--- src/utils/customUtils.ts
+++ src/utils/customUtils.ts
@@ -1,4 +1,5 @@
 import { Vector2, Vector3 } from "three";
+import * as Three from 'three';
 
 export function getAngleBetweenVectors(first: Vector2, second: Vector2): number{
     let angle = Math.acos((first.x * second.x + first.y * second.y)/Math.sqrt((Math.pow(first.x, 2) + Math.pow(second.x, 2)) * (Math.pow(first.y, 2) + Math.pow(second.y, 2))));
@@ -17,4 +18,17 @@ export function isFrontSide(raycaster : THREE.Raycaster, camera : THREE.Perspect
     const intersections = raycaster.intersectObject(model, true);
     if((Math.abs(intersections[0].point.x - objectPosition.x )< 0.1)&& (Math.abs(intersections[0].point.y - objectPosition.y ) < 0.1)&&(Math.abs(intersections[0].point.z - objectPosition.z ) < 0.1))return true;
     return isFront;
+}
+
+export function getCurrentCameraTarget(raycaster : THREE.Raycaster, camera : THREE.PerspectiveCamera, model: THREE.Object3D):any{
+    raycaster.setFromCamera(
+        {
+            x: 0,
+            y: 0
+        },
+        camera
+    );
+    let isFront = false;
+    const intersections = raycaster.intersectObject(model, true);
+    return intersections.length? new Three.Vector3(intersections[0].point.x, intersections[0].point.y, intersections[0].point.z): 'No match';
 }
